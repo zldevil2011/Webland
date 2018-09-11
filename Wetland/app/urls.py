96c31cbc47e55f12bf31e7bd53748e50d380e9b6
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2018-09-11 23:29:49 +0800

    feat: add urls map

--- Wetland/app/urls.py
+++ Wetland/app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
-from app.views import index
+from app.views.index import index
 
 urlpatterns = [
-    url(r'index/$',index, name="index")
+    url('index/$',index, name="index")
 ]
